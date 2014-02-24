d9f4b3efff3d7f5ce8ef32e4fb6c602be70d8a90
Dylan Nelson <dynelson@au1.ibm.com>
2014-02-24 14:19:47 +1100

    team stats view

---
+++ QuiltDemo/RFGraphCollectionView.m
@@ -0,0 +1,31 @@
+//
+//  RFGraphCollectionView.m
+//  Waratahs
+//
+//  Created by IBM Australia on 19/02/14.
+//  Copyright (c) 2014 Bryce Redd. All rights reserved.
+//
+
+#import "RFGraphCollectionView.h"
+
+@implementation RFGraphCollectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ QuiltDemo/RFGraphsCell.h
@@ -0,0 +1,13 @@
+//
+//  RFGraphsCell.h
+//  Waratahs
+//
+//  Created by IBM Australia on 19/02/14.
+//  Copyright (c) 2014 Bryce Redd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFGraphsCell : UICollectionViewCell
+
+@end
