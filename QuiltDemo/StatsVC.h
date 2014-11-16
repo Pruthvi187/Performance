7a7f3060395ac91c29e5fe332f0cdd9a3a4b2ea7
Pruthvi <pruthvi187@gmail.com>
2014-11-16 13:08:15 +1100

    Fixes for iOS 8

---
+++ QuiltDemo/StatsVC.h
@@ -0,0 +1,13 @@
+//
+//  StatsVC.h
+//  Waratahs
+//
+//  Created by Pruthvi on 14/11/2014.
+//  Copyright (c) 2014 Bryce Redd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatsVC : UIViewController
+
+@end
